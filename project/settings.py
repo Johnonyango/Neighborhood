c7f74c9dea487ff31602c974b9f60518a9e3ab51
Johnonyango <j.yayah7@gmail.com>
2019-09-14 23:12:08 +0300

    modify settings.py

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap3',
     'django.contrib.admin',
     'neighborhood.apps.NeighborhoodConfig',
     'django.contrib.auth',
