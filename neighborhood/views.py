7014bf607ff90c9fc8e8322b288e9a650ef86037
Johnonyango <j.yayah7@gmail.com>
2019-09-14 23:10:18 +0300

    create base.html

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -4,36 +4,45 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 # Create your views here.
 def welcome(request):
-    return render(request,'index.html')
+    id = request.user.id
+    profile = Profile.objects.get(user=id)
+    return render(request, 'index.html',{'profile':profile})
 
 def login(request):
     return render(request, 'registration/registration.html')
 
-def profile(request):
-    return render(request, 'profile.html')
+def profile(request, id):
+    user = request.user.id
+    profile = Profile.objects.get(user=user)
+    user = request.user
+    neighbourhoods = Neighbourhood.objects.filter(post=frank).order_by()
+    neighbourhoodcount=neighbourhoods.count()
+    
+    return render(request, 'profile.html',{'profile':profile,'user':user})
+
 
 def business(request):
-  john = request.user.id
-  profile = Profile.objects.get(user=john)
+    john = request.user.id
+    profile = Profile.objects.get(user=john)
 
-  if request.method == 'POST':
-    form = NewBusinessForm(request.POST)
-    if form.is_valid():
-      business = form.save(commit=False)
-      business.neighbourhood = profile.neighbourhood
-      business.save()
+    if request.method == 'POST':
+        form = NewBusinessForm(request.POST)
+        if form.is_valid():
+        business = form.save(commit=False)
+        business.neighbourhood = profile.neighbourhood
+        business.save()
     return redirect('business')
 
-  else:
-    form = NewBusinessForm()
+    else:
+        form = NewBusinessForm()
 
-  return render(request, 'business.html',{'form':form,'profile':profile})
+    return render(request, 'business.html',{'form':form,'profile':profile})
 
 
-  else:
-    form = NewBusinessForm()
+#   else:
+#     form = NewBusinessForm()
 
-    return render(request, 'businesses.html')
+#     return render(request, 'businesses.html')
 
 def services(request):
     return render(request, 'govern.html')
