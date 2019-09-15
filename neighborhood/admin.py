54003ff34c87c0be18343af12e3661e81e323646
Johnonyango <j.yayah7@gmail.com>
2019-09-15 07:09:02 +0300

    update settings.py

--- neighborhood/admin.py
+++ neighborhood/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Neighbourhood, Business, Contact,Post,Comment
+
 
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Post)
+admin.site.register(Business)
+admin.site.register(Contact)
+admin.site.register(Comment)
--- neighborhood/models.py
+++ neighborhood/models.py
@@ -1,3 +1,101 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 
 # Create your models here.
+class Neighbourhood(models.Model):
+  name = models.CharField(max_length=60,default='name')
+  image = models.ImageField(upload_to = 'photos/')
+  location = models.CharField(max_length=60)
+  occupants_count = models.PositiveIntegerField()
+  email = models.EmailField(max_length=60,default='email')
+  description = models.TextField(default='description')
+  pub_date = models.DateTimeField(auto_now_add=True)
+
+
+  def __str__(self):
+    return self.name
+  class Meta:
+    ordering = ['name']
+
+  def save_neighbourhood(self):
+    self.save()
+
+  def delete_neighbourhood(self):
+    self.delete()
+
+
+class Business(models.Model):
+  name = models.CharField(max_length=60)
+  email = models.EmailField()
+  neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+  pub_date = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.name
+  class Meta:
+    ordering = ['name']
+
+  def save_business(self):
+    self.save()
+
+  def delete_business(self):
+    self.delete()
+
+
+
+
+class Profile(models.Model):
+  photo = models.ImageField(upload_to = 'photos/')
+  bio = models.CharField(max_length=200)
+  contact = models.EmailField()
+  neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE,blank=True,null=True)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+  @receiver(post_save, sender=User)
+  def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+      Profile.objects.create(user=instance)
+
+  @receiver(post_save, sender=User)
+  def save_user_profile(sender, instance, **kwargs):
+      instance.profile.save()
+
+  post_save.connect(save_user_profile, sender=User)
+
+  def save_profile(self):
+    self.save()
+
+  def delete_profile(self):
+    self.delete()
+
+
+
+
+
+class NeighLetterRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()    
+
+class Comment(models.Model):
+  comment = models.TextField()
+  neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+  postername = models.CharField(max_length=60)
+  pub_date = models.DateTimeField(auto_now_add=True)
+
+class Post(models.Model):
+  name = models.CharField(max_length=60)
+  post = models.TextField()
+  poster = models.ForeignKey(User,on_delete=models.CASCADE)
+  postername = models.CharField(max_length=60)
+  neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+  pub_date = models.DateTimeField(auto_now_add=True)
+
+class Contact(models.Model):
+  name = models.CharField(max_length=140)
+  contact = models.CharField(max_length=60)
+  email = models.EmailField()
+  neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+  pub_date = models.DateTimeField(auto_now_add=True)
