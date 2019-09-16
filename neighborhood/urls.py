507bf22260221be5f52603cdbadb07e75cb90377
Johnonyango <j.yayah7@gmail.com>
2019-09-16 09:20:31 +0300

    update url paths

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -1,12 +1,33 @@
-from django.urls import path
-from.import views
+from django.conf.urls import url
+from django.conf.urls.static import static
+from .import views
+from django.conf import settings
+from django.core.urlresolvers import reverse
 
-urlpatterns = [
-    path('', views.welcome, name = 'welcome'),
-    path('profile/', views.profile, name='profile'),
-    path('businesses/', views.business, name='businesses'),
-    path('services/', views.services, name='services'),
-    path('posts/', views.posts, name='posts'),
-    path('change/', views.change_hood, name='change_hood'),
 
-]
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome'),
+    url(r'^contacts/', views.contacts, name='contacts'),
+    url(r'^search/', views.search,name = 'search'),
+    url(r'^new/neighbourhood$',views.newneighbourhood, name='newneighbourhood'),
+    url(r'^profile/(\d+)', views.profile, name='profile'),
+    url(r'^neighbourhood/(\d+)', views.neighbourhood, name='neighbourhood'),
+    url(r'^new/business/',views.newbusiness, name='newbusiness'), 
+    url(r'^new/profile$',views.newprofile, name='newprofile'),
+    url(r'^new/comment/',views.newcomment, name='newcomment'), 
+    url(r'^new/post$',views.newpost, name='newpost'),
+    url(r'^mail$',views.mail,name='mail'),
+    url(r'^chat/', views.chat, name='chat'),
+    url(r'^subscribe/', views.subscribe, name='subscribe'),
+    url(r'^business$', views.business,name = 'business'),
+    url(r'^myneighbourhood/', views.myneighbourhood, name='myneighbourhood'),
+    url(r'^password/', views.password, name='password'),
+    url(r'^new/contacts/',views.newcontacts, name='newcontacts'), 
+    url(r'^post/(\d+)', views.post, name='post'),
+    url(r'^mypost/', views.mypost, name='mypost'),
+    url(r'^comments/', views.comments, name='comments'),
+
+]    
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
